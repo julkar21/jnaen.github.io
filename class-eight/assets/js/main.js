89c3359734914a12d4e1cfdbfbb5b7b1d32eab8a
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-07-27 20:18:22 +0600

    class-eight

---
+++ class-eight/assets/js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    $(".product-carousel").owlCarousel({
+            
+            items: 4,
+            margin: 30,
+            loop: true,
+            nav: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+           
+        });    
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
