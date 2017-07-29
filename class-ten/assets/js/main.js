62f1a027a47ef41742dc323b39bda35ceba1e845
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-07-29 22:47:13 +0600

    class-ten

---
+++ class-ten/assets/js/main.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+        $(".homepage-slides").owlCarousel({
+            
+            items: 1,
+            loop: true,
+            nav: true,
+            navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+           
+        });    
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
