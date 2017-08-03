5efeb3453a63e5a103e67495c0d31fab404c8ffc
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-08-03 23:45:14 +0600

    class-fourteen

---
+++ class-fourteen/assets/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    $(".portfolio-item-list").masonry({
+        
+        horizontalOrder: true,
+        
+    });    
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
