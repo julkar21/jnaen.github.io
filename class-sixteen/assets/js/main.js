6f345d9ac7299e7bf8c0323f96dec0434b1bc188
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-08-06 19:57:13 +0600

    class-sixteen

---
+++ class-sixteen/assets/js/main.js
@@ -0,0 +1,59 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    $(".portfolio-item-list").masonry({
+        
+        horizontalOrder: true,
+        
+    });  
+        
+        
+  $(".portfolio-filter li").on('click', function(){
+      
+      var filterData = $(this).attr("data-filter");
+    
+        $(".portfolio-item-list").isotope({
+            filter: filterData,
+            
+        }); 
+      
+      $(".portfolio-filter li").removeClass('active');
+      $(this).addClass('active');
+        
+    });
+        
+        
+    
+        $(".portfolio-item-list").isotope({
+           
+            itemSelector: '.singel-portfolio',
+            percentPosition: true,
+            
+            masonry: {
+            
+                columnWidth: '.singel-portfolio',
+                horizontalOrder: true
+            }
+            
+        });
+            
+        
+    $("body").perfectScrollbar({
+             wheelSpeed:2,
+            maxScrollbarLength:50	
+    });
+    $(".text-bar").perfectScrollbar({
+        theme:"barGreen", 
+    });
+        
+        
+
+    });
+
+
+   
+
+
+}(jQuery));	
