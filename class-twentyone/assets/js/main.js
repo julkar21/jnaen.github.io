e629d8f90f1f6bd248f379c76672216ae6793c40
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-08-11 23:44:29 +0600

    class-twentyone

---
+++ class-twentyone/assets/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+     $('.schedule').DataTable();
+        
+        });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
