56a6ebec3fae2d95a074de1bbf5f6b6f390bd4a0
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-08-14 11:54:33 +0600

    class-twentytwo

---
+++ class-twentytwo/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    $(".portfolio-items").owlCarousel({
+          items: 5,
+        loop: true,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        dots: true, 
+    });    
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
