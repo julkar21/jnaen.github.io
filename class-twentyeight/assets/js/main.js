b4345d100a8b05e4750475b012f9c16979876d00
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-08-20 21:51:32 +0600

    class-twentyeight

---
+++ class-twentyeight/assets/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".portfolio-area").masonry({
+            
+        });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
