d9a0b7e44e96c9b0103c800b28356b5328dfc2d2
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-09-10 12:45:26 +0600

    class-thirtytwo

---
+++ class-thirtytwo/assets/js/main.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+    $(".video-play-btn").modalVideo();    
+        
+    $(".video-play-btn").on('click', function(){
+        return false;
+    });    
+        
+
+    });
+
+
+}(jQuery));	
