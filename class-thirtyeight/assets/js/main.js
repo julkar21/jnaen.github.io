d0e800f5c417e7d61b7100c21d0aa696985c9b05
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2017-10-04 17:49:01 +0600

    class-thirtyeight

---
+++ class-thirtyeight/assets/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+     $('.count').counterUp({
+                delay: 10,
+                time: 1000
+            });    
+
+        
+
+
+    });
+
+
+
+
+}(jQuery));	
