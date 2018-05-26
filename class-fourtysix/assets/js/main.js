16531cddf0b8d12898647d12ceba04d1234612b1
Md Julkar Naen <mdjulkarnaen7@gmail.com>
2018-05-26 22:16:59 +0600

    class-fourtysix

---
+++ class-fourtysix/assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+    $(".slider-area").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: true
+    });
+    
+});
